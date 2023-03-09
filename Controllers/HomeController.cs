9861cd41a9e7cfd704e150bc0e7d19f4ec1295fc
mohammad ayesh <mohammad.ayeshh@gmail.com>
2023-03-09 15:21:37 +0300

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using jam3ea.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace jam3ea.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
